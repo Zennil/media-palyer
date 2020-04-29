7670d6d993c09cb143de0a4fda6f23ecf6875505
Armando Zenil <szenil.11@gmail.com>
2020-04-29 12:29:36 -0500

    Se agregan algunos ejemplos de tipos de dato en TypeScript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,38 @@
-console.log('Hey, TypeScript');
+// Boolean
+let muted: boolean = true;
+muted = false;
 
-function add(a: number, b: number): number {
-    return a + b;
-}
+// Números
+let age = 6;
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador / denominador;
 
-const sum = add(2, 3);
+// String
+let nombre: string = 'Zenil';
+let saludo: string = `Me llamo ${nombre}`;
+
+// Arreglo
+let people: string[] = [];
+people = ['Asimov', 'Dorian', 'Zack'];
+people.push(nombre);
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push('Bukousky');
+peopleAndNumbers.push(7001);
+
+// Enum
+enum Color {
+    Rojo = 'rojo',
+    Verde = 'verde',
+    Azul = 'azul',
+    Amarrillo = 'amarillo'
+};
+let colorFavorito: Color = Color.Rojo;
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+// Any
+let comodin: any = 'Joker';
+comodin = { type: 'WildCard' };
+
+// Object
+let someObject: Object = { type: 'Wildcard' };
