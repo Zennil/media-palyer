1e90053815d5aed5baa58188338ee251734c8608
Armando Zenil <szenil.11@gmail.com>
2020-05-04 11:38:37 -0500

    Implementacion de plugin de Ads: Desplegado en pantalla

--- assets/index.ts
+++ assets/index.ts
@@ -34,13 +34,13 @@ btnMuted.onclick = () => {
 }
 
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(function (registration) {
-            // Si es exitoso
-            console.log('SW registrado correctamente');
-        }, function (err) {
-            // Si falla
-            console.log('SW fallo', err);
-        });
+    // window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
+        // Si es exitoso
+        console.log('SW registrado correctamente');
+    }, function (err) {
+        // Si falla
+        console.log('SW fallo', err);
     });
+    // });
 }
